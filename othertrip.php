5f8ae14e3a65ea9eda019607423028ee9f2ee350
leocahuzac <leocahuzac@gmail.com>
2016-10-04 23:21:32 +0200

    Rajout des pages
    …

---
+++ othertrip.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ouch</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href= "style.css"/>
+    </head>
+    <header>
+        <div class="title">
+          <img src="images/header1.jpeg" alt="title">
+        </div>
+    </header>
+    <body>
+        <div class="main"> 
+          
+        </div>
+    </body>
+</html>
