b6b3f7534055accf2099f284b497839ef5a9a985
leocahuzac <leocahuzac@gmail.com>
2016-10-05 00:49:40 +0200

    Onglet
    rajout des onglets sur toutes les pages
    onglets links
    titre site linké

--- hanoue.php
+++ hanoue.php
@@ -7,7 +7,18 @@
     </head>
     <header>
         <div class="title">
-          <img src="images/header1.jpeg" alt="title">
+          <a href="index.php"><img src="images/header1.png" alt="title"></a>
+        </div>
+        <div class="onglets">
+            <a href="strip.php"> <img class="onglet1" src="images/ongletStrip.png" alt="onglet1"></a>
+            <a href="othertrip.php"><img class="onglet1" src="images/ongletOthertrip.png" alt="onglet2"></a>
+            <a href="video.php"><img class="onglet1" src="images/ongletVideo.png" alt="onglet3"></a>
+            <a href="hanoue.php"><img class="onglet1" src="images/ongletHanoue.png" alt="onglet4"></a>
+            <a href="infos.php"><img class="onglet1" src="images/ongletInfos.png" alt="onglet5"></a>
+        </div>
+
+        <div class="onglet2">
+            
         </div>
     </header>
     <body>
