e3e4d22c56862b6825b4a543194dbe55be9fe18e
leocahuzac <leocahuzac@gmail.com>
2016-10-09 17:06:18 +0200

    Commit général

--- contact.php
+++ contact.php
@@ -11,14 +11,11 @@
         </div>
         <div class="onglets">
             <a href="strip.php"> <img class="onglet1" src="images/ongletStrip.png" alt="onglet1"></a>
-            <a href="othertrip.php"><img class="onglet1" src="images/ongletOthertrip.png" alt="onglet2"></a>
-            <a href="video.php"><img class="onglet1" src="images/ongletVideo.png" alt="onglet3"></a>
-            <a href="hanoue.php"><img class="onglet1" src="images/ongletHanoue.png" alt="onglet4"></a>
-            <a href="infos.php"><img class="onglet1" src="images/ongletInfos.png" alt="onglet5"></a>
-        </div>
-
-        <div class="onglet2">
-            
+            <a href="othertrip.php"><img class="onglet2" src="images/ongletOthertrip.png" alt="onglet2"></a>
+            <a href="video.php"><img class="onglet3" src="images/ongletVideo.png" alt="onglet3"></a>
+            <a href="hanoue.php"><img class="onglet4" src="images/ongletHanoue.png" alt="onglet4"></a>
+            <a href="infos.php"><img class="onglet5" src="images/ongletInfos.png" alt="onglet5"></a>
+            <a href="infos.php"><img class="onglet6" src="images/ongletContact.png" alt="onglet6"></a>
         </div>
     </header>
     <body>
